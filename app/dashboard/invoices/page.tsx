dc88c599e606da76fb5de9684313ce5d5dac4770
ericabirgit <erl00019@laurea.fi>
2026-05-10 19:12:40 +0300

    2

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,11 @@
-import { invoices } from '@/app/lib/placeholder-data';
+
 import InvoicesTable from '@/app/ui/invoices/table';
 
 export default function InvoicesPage() {
   return (
     <main>
       <h1>Invoices</h1>
-      <InvoicesTable invoices={invoices} />
+      <InvoicesTable query="" currentPage={1} />
     </main>
   );
 }
